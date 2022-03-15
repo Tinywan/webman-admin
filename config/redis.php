a8542b1b407f7685ba7aae2bb635ac11dec44120
万少波 <756684177@qq.com>
2022-03-15 14:13:32 +0800

    default redis docker address

--- config/redis.php
+++ config/redis.php
@@ -14,8 +14,8 @@
 
 return [
     'default' => [
-        'host'     => '127.0.0.1',
-        'password' => null,
+        'host'     => 'dnmp-redis',
+        'password' => 123456,
         'port'     => 6379,
         'database' => 0,
     ],

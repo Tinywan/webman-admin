d1565669a19cf51a94794b2c14ac416f99705022
Tinywan <756684177@qq.com>
2023-10-28 22:34:50 +0800

    feat:workbunny/webman-nacos

---
+++ config/plugin/workbunny/webman-nacos/channel.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+return [
+    'default' => [
+        'host' => '192.168.1.2',
+        'port' => 8848,
+        'username' => 'nacos',
+        'password' => 'nacos',
+    ],
+];

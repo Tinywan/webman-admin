869cdc73b053fe26c50b057dc65a0c341800fc6e
万少波 <756684177@qq.com>
2023-12-27 10:23:23 +0800

    fix: JavaScript电子表格处理神器 SheetJS

---
+++ config/plugin/webman/push/app.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'enable'       => true,
+    'websocket'    => 'websocket://0.0.0.0:3131',
+    'api'          => 'http://0.0.0.0:3232',
+    'app_key'      => 'c828cd6d3ec1365037ec33d484524596',
+    'app_secret'   => '35928da47b51cc4641ac89b7aedf6195',
+    'channel_hook' => 'http://127.0.0.1:8787/plugin/webman/push/hook',
+    'auth'         => '/plugin/webman/push/auth'
+];
